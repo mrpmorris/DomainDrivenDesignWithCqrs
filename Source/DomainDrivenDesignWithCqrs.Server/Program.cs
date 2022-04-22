095d5e41360e5a9c803996c9d7c6b1bba8bfc064
mrpmorris <mrpmorris@gmail.com>
2022-04-22 15:47:41 +0100

    Small code formatting

--- Source/DomainDrivenDesignWithCqrs.Server/Program.cs
+++ Source/DomainDrivenDesignWithCqrs.Server/Program.cs
@@ -1,26 +1,21 @@
 using System.Text.Json.Serialization;
+using AppLayerServices = DomainDrivenDesignWithCqrs.AppLayer.Services.Registration;
 using DomainDrivenDesignWithCqrs.Server.Extensions;
 using Microsoft.AspNetCore.Http.Json;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.Configure<JsonOptions>(x => 
 		{
 			x.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
 			x.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
 		});
-
-DomainDrivenDesignWithCqrs.AppLayer.Services.Registration.Register(
-	builder.Services,
-	builder.Configuration);
-
+AppLayerServices.Register(builder.Services, builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 app.MapApplicationApiEndpoints();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 	app.UseWebAssemblyDebugging();
@@ -30,10 +25,8 @@ if (app.Environment.IsDevelopment())
 else
 {
 	app.UseExceptionHandler("/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
